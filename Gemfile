406a103e99f2a6a052597a1300db3c218fc683e2
William Wingate <wtwingate@gmail.com>
2025-03-08 12:23:34 -0500

    Finish Ruby Event Manager tutorial

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@
 source 'https://rubygems.org'
 
 gem 'rubocop', require: false
+
+gem 'google-apis-civicinfo_v2', '~> 0.24.0'
+
+gem 'csv', '~> 3.3'
--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,3 +1,59 @@
 # frozen_string_literal: true
 
+require 'csv'
+require 'erb'
+
+require 'google/apis/civicinfo_v2'
+
+def clean_zipcode(zipcode)
+  zipcode.to_s.rjust(5, '0')[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+  civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
+  civic_info.key = File.read('secret.key').strip
+
+  begin
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
+  rescue Google::Apis::ClientError
+    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+exit unless File.exist? 'event_attendees.csv'
+
 puts 'Event Manager Initialized!'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+template_letter = File.read('form_letter.html.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
+end
